38100e49fb7f552dbeb7931657f025c113fff0f4
noreply-gtisec <noreply@gtisec.com>
2026-05-21 15:32:58 +0530

    changes

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
